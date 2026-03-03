46ae42adff08136e992604adb19be61aa02af39c
davnotdev <contact@davnot.dev>
2026-03-03 12:24:19 -0800

    Fixup cli for name changes

--- cli/cli.c
+++ cli/cli.c
@@ -32,25 +32,25 @@ int main(int argc, char **argv) {
 
 	// --- front
 
-	CapabilitiesFlags caps =
-			Capabilities_MULTISAMPLED_SHADING |
-			Capabilities_CUBE_ARRAY_TEXTURES |
-			Capabilities_IMMEDIATES |
-			Capabilities_STORAGE_TEXTURE_16BIT_NORM_FORMATS |
-			Capabilities_SHADER_FLOAT16_IN_FLOAT32 |
-			Capabilities_TEXTURE_AND_SAMPLER_BINDING_ARRAY |
-			Capabilities_TEXTURE_AND_SAMPLER_BINDING_ARRAY_NON_UNIFORM_INDEXING |
-			Capabilities_STORAGE_TEXTURE_BINDING_ARRAY |
-			Capabilities_STORAGE_TEXTURE_BINDING_ARRAY_NON_UNIFORM_INDEXING |
-			Capabilities_SUBGROUP;
-	ModuleFillFlags fill_flags = NAGA_FLAGS_ALL(ModuleFillFlags);
-	SPVFrontOptions options = (SPVFrontOptions){
+	NagaCapabilitiesFlags caps =
+			NagaCapabilities_MULTISAMPLED_SHADING |
+			NagaCapabilities_CUBE_ARRAY_TEXTURES |
+			NagaCapabilities_IMMEDIATES |
+			NagaCapabilities_STORAGE_TEXTURE_16BIT_NORM_FORMATS |
+			NagaCapabilities_SHADER_FLOAT16_IN_FLOAT32 |
+			NagaCapabilities_TEXTURE_AND_SAMPLER_BINDING_ARRAY |
+			NagaCapabilities_TEXTURE_AND_SAMPLER_BINDING_ARRAY_NON_UNIFORM_INDEXING |
+			NagaCapabilities_STORAGE_TEXTURE_BINDING_ARRAY |
+			NagaCapabilities_STORAGE_TEXTURE_BINDING_ARRAY_NON_UNIFORM_INDEXING |
+			NagaCapabilities_SUBGROUP;
+	NagaModuleFillFlags fill_flags = NAGA_FLAGS_ALL(NagaModuleFillFlags);
+	NagaSPVFrontOptions options = (NagaSPVFrontOptions){
 		.adjust_coordinate_space = 1,
 		.strict_capabilities = 1,
 		.block_ctx_dump_prefix = NULL,
 	};
-	SPVFrontResult front_result;
-	front_result.flags = FrontResultOption_FormattedErrorOnly;
+	NagaSPVFrontResult front_result;
+	front_result.flags = NagaFrontResultOption_FormattedErrorOnly;
 	success = naga_front_spv_parse(options, (uint32_t *)buffer, input_size / 4, fill_flags, &front_result);
 	if (!success) {
 		printf("Front error: %s\n", front_result.fmt_error);
@@ -59,9 +59,9 @@ int main(int argc, char **argv) {
 
 	// --- valid
 
-	Validator validator = naga_valid_validator_new(NAGA_FLAGS_ALL(ValidationFlagsFlags), caps);
-	ValidateResult valid_result;
-	valid_result.flags = ValidateResultOption_FormattedErrorOnly;
+	NagaValidator validator = naga_valid_validator_new(NAGA_FLAGS_ALL(NagaValidationFlagsFlags), caps);
+	NagaValidateResult valid_result;
+	valid_result.flags = NagaValidateResultOption_FormattedErrorOnly;
 	success = naga_valid_validator_validate(&validator, &front_result.module, &valid_result);
 	if (!success) {
 		printf("Valid error: %s\n", valid_result.fmt_error);
@@ -70,9 +70,9 @@ int main(int argc, char **argv) {
 
 	// --- back
 
-	WGSLBackWriterFlagsFlags writer_flags = NAGA_FLAGS_EMPTY(WGSLBackWriterFlagsFlags);
-	WGSLWriteResult back_result;
-	back_result.flags = WriteResultOption_FormattedErrorOnly;
+	NagaWGSLBackWriterFlagsFlags writer_flags = NAGA_FLAGS_EMPTY(NagaWGSLBackWriterFlagsFlags);
+	NagaWGSLWriteResult back_result;
+	back_result.flags = NagaWriteResultOption_FormattedErrorOnly;
 	success = naga_back_wgsl_write(&front_result.module, &valid_result.module_info, writer_flags, &back_result);
 	if (!success) {
 		printf("Back error: %s\n", back_result.fmt_error);
